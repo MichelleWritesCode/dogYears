22e1a326f6c37294553e754597abd086154b80d1
Michelle <michellevanwijk.00@gmail.com>
2020-09-11 12:18:08 +0200

    Import

---
+++ app.js
@@ -0,0 +1,19 @@
+// changable variable with my age
+let myAge = 29;
+
+// changable variable early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// my age minus the value of early years
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+// calculating my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// my name written in lowercase
+let myName = 'Michelle'.toLowerCase();
+
+// logging my age in dog years with the values of above
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
